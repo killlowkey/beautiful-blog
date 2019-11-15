cf7b58a26419af057ed540e8e2e45ed8e5bbb68e
killlowkey <2860072080@qq.com>
2019-11-15 20:00:22 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,16 @@
+# 小而美的博客系统
+
+## 开发环境
+* Idea
+* Maven
+* Jdk8
+* SpringBoot 2.2.1
+
+## SpringBoot 模块
+* Thymeleaf
+* Jpa
+* Lombok
+* Aop
+* MySQL
+* DevTools
+
---
+++ src/main/java/com/predicated/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.predicated.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ray
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
